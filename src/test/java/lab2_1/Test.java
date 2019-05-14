b46d87d08b50374fce57829bd0b257ca3292ba2d
krulik40000 <michalmalis@o2.pl>
2019-05-14 10:29:13 +0200

    testy_v1
    test czy elementu nie ma w sekwencji

--- src/test/java/lab2_1/Test.java
+++ src/test/java/lab2_1/Test.java
@@ -9,12 +9,18 @@ import static edu.iis.mto.bsearch.BinarySearch.search;
 public class Test {
 
     private int singleElementSeq[] = new int[1];
-    
+
     @org.junit.Test
     public void SingleElementInSeq() {
         SearchResult result = search(singleElementSeq[0], singleElementSeq);
-        assertThat(result.isFound(),is(true));
-        assertThat(result.getPosition(),is(1));
+        assertThat(result.isFound(), is(true));
+        assertThat(result.getPosition(), is(1));
     }
 
+    @org.junit.Test
+    public void NotInSeq() {
+        SearchResult result = search(singleElementSeq[0] + 1, singleElementSeq);
+        assertThat(result.isFound(), is(false));
+        assertThat(result.getPosition(), is(-1));
+    }
 }
