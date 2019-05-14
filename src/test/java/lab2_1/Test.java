8188f66eec42d1131852a363ce088a19a039cd17
krulik40000 <michalmalis@o2.pl>
2019-05-14 10:35:12 +0200

    testy_v1
    test czy element jest pierwszy w sekwencji

--- src/test/java/lab2_1/Test.java
+++ src/test/java/lab2_1/Test.java
@@ -1,6 +1,9 @@
 package lab2_1;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
+
 import static org.hamcrest.core.Is.is;
 
 import edu.iis.mto.bsearch.SearchResult;
@@ -9,6 +12,16 @@ import static edu.iis.mto.bsearch.BinarySearch.search;
 public class Test {
 
     private int singleElementSeq[] = new int[1];
+    private int seq[] = new int[20];
+
+    @Before
+    public void initTest() {
+        for (int i = 0; i < seq.length; i++) {
+            seq[i] = i;
+        }
+
+        singleElementSeq[0] = 1;
+    }
 
     @org.junit.Test
     public void SingleElementInSeq() {
@@ -22,5 +35,12 @@ public class Test {
         SearchResult result = search(singleElementSeq[0] + 1, singleElementSeq);
         assertThat(result.isFound(), is(false));
         assertThat(result.getPosition(), is(-1));
+    }
+    
+    @org.junit.Test
+    public void FirstElementInSeq() {
+        SearchResult result = search(seq[0], seq);
+        assertThat(result.isFound(), is(true));
+        assertThat(result.getPosition(), is(1));
     }
 }
